b42546b0394f61079ca574d070a945277b96cb6a
Thodoris Lazaris <117118038+Lazaris02@users.noreply.github.com>
2023-12-07 12:33:55 +0200

    many changes
    added bpm changed timeManager 1line
    +bar animator last if statement

--- src/barAnimationManager.js
+++ src/barAnimationManager.js
@@ -6,6 +6,7 @@ let call_animation; // in milliseconds -- will change from main-thread
 self.onmessage = (e)=>{
     if(e.data == 'start'){
         console.log('starting animation');
+        postMessage('animate');
         animation_interval = setInterval(()=>{postMessage('animate')},call_animation); 
     }
     else if(e.data == 'stop'){
@@ -16,5 +17,10 @@ self.onmessage = (e)=>{
     else if(e.data[0] == 'change'){
         console.log('changing animation timing');
         call_animation = e.data[1]*1000; // convert the data into milliseconds
+        if(animation_interval){
+            clearInterval(animation_interval);
+            animation_interval = setInterval(()=>{postMessage('animate')},call_animation);
+            console.log('set new animation interval!')
+        }
     }
 }
